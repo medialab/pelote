d9e6a69c483634dcd5c6ca841481275a66a5149f
Yomguithereal <guillaumeplique@gmail.com>
2022-03-02 17:24:07 +0100

    node_key_col

--- pelote/network_to_tabular.py
+++ pelote/network_to_tabular.py
@@ -4,17 +4,31 @@
 #
 # Functions able to convert networkx to various tabular data formats.
 #
-from typing import Any
+from typing import Any, Optional
 
 from pelote.types import AnyGraph
 from pelote.shim import pd, check_pandas
 
 
-def to_nodes_dataframe(graph: "AnyGraph[Any]") -> pd.DataFrame:
+def to_nodes_dataframe(
+    graph: "AnyGraph[Any]", node_key_col: Optional[str] = "key"
+) -> pd.DataFrame:
     check_pandas()
 
-    iterator = (a for _, a in graph.nodes(data=True))
+    if node_key_col is None:
 
-    df = pd.DataFrame(data=iterator, index=list(graph.nodes))
+        def raw_data():
+            for _, a in graph.nodes(data=True):
+                yield a
 
-    return df
+        return pd.DataFrame(data=raw_data(), index=list(graph.nodes))
+
+    else:
+
+        def data_with_key():
+            for n, a in graph.nodes(data=True):
+                r = {node_key_col: n}
+                r.update(a)
+                yield r
+
+        return pd.DataFrame(data=data_with_key())
--- test/network_to_tabular_test.py
+++ test/network_to_tabular_test.py
@@ -10,18 +10,41 @@ from pelote.shim import missing_pandas
 from pelote.network_to_tabular import to_nodes_dataframe
 
 
-class TestNetworkToTabular(object):
-    def test_to_nodes_dataframe(self):
+def get_basic_range_graph(n: int = 3) -> "nx.Graph[int]":
+    g: nx.Graph[int] = nx.Graph()
+    g.add_nodes_from(range(n))
+
+    for i, node in enumerate(g):
+        g.nodes[node]["value"] = i
+
+    return g
+
+
+class TestToNodesDataframe(object):
+    def test_missing_pandas(self):
         with raises(MissingPandasException), missing_pandas():
             to_nodes_dataframe(nx.Graph())
 
-        g: nx.Graph[int] = nx.Graph()
-        g.add_nodes_from(range(3))
-
-        for i, node in enumerate(g):
-            g.nodes[node]["value"] = i
+    def test_default_behavior(self):
+        g = get_basic_range_graph()
 
         df = to_nodes_dataframe(g)
+        expected = pd.DataFrame(data={"key": list(range(3)), "value": list(range(3))})
+
+        assert df.equals(expected)
+
+    def test_node_key_col(self):
+        g = get_basic_range_graph()
+
+        df = to_nodes_dataframe(g, node_key_col="node")
+        expected = pd.DataFrame(data={"node": list(range(3)), "value": list(range(3))})
+
+        assert df.equals(expected)
+
+    def test_node_key_as_index(self):
+        g = get_basic_range_graph()
+
+        df = to_nodes_dataframe(g, node_key_col=None)
         expected = pd.DataFrame(index=list(range(3)), data={"value": list(range(3))})
 
         assert df.equals(expected)
