dd62d1440e0cd5826d0ac1ee9274fd4eff1e9c1a
bmaz <beatrice.mazoyer@gmail.com>
2022-03-04 18:18:48 +0100

    #3 edge disparity metric

---
+++ pelote/metrics.py
@@ -0,0 +1,60 @@
+# =============================================================================
+# Pelote Metrics Functions
+# =============================================================================
+#
+import networkx as nx
+import math
+from typing import Dict
+from pelote.types import AnyGraph
+
+
+def edge_disparity(graph: AnyGraph) -> Dict:
+    """
+    Function computing the disparity score of each edge in the given graph. This
+    score is typically used to extract the multiscale backbone of a weighted
+    graph.
+    Args:
+        graph(nx.AnyGraph): target graph.
+
+    Returns:
+        dict: Dictionnary with edges - (source, target) tuples - as keys and the disparity scores as values
+
+    """
+    # todo: raise if multigraph
+
+    weighted_degrees = dict.fromkeys(graph, 0.0)
+
+    for source, target, data in graph.edges(data=True):
+        weighted_degrees[source] += data["weight"]
+        weighted_degrees[target] += data["weight"]
+
+    disparities = {}
+    first_edge = True
+
+    for source, target, data in graph.edges(data=True):
+
+        if first_edge:
+            previous = source
+            previous_degree = graph.degree(source)
+            previous_weighted_degree = weighted_degrees[source]
+            first_edge = False
+
+        if previous != source:
+            previous = source
+            previous_degree = graph.degree(source)
+            previous_weighted_degree = weighted_degrees[source]
+
+        target_degree = graph.degree(target)
+        target_weighted_degree = weighted_degrees[target]
+
+        weight = data["weight"]
+
+        normalized_weight_source = weight / previous_weighted_degree
+        normalized_weight_target = weight / target_weighted_degree
+
+        source_score = math.pow(1 - normalized_weight_source, previous_degree - 1)
+        target_score = math.pow(1 - normalized_weight_target, target_degree - 1)
+
+        disparities[(source, target)] = min(source_score, target_score)
+
+    return disparities
---
+++ test/metrics_test.py
@@ -0,0 +1,58 @@
+# =============================================================================
+# Pelote Metrics Unit Tests
+# =============================================================================
+import networkx as nx
+
+from pelote.metrics import (
+    edge_disparity
+)
+
+
+class TestEdgeDisparity(object):
+    def test_identical_values(self):
+        g = nx.Graph()
+        g.add_weighted_edges_from(
+            [
+                (0, 1, 1),
+                (0, 2, 1),
+                (0, 3, 1),
+                (1, 2, 1),
+                (1, 3, 1),
+                (2, 3, 1)
+            ]
+        )
+        disparities = edge_disparity(g)
+        for i, disparity in enumerate(disparities.values()):
+            if i == 0:
+                previous_disparity = disparity
+            assert previous_disparity == disparity
+            previous_disparity = disparity
+
+    def test_correct_disparities(self):
+        g = nx.complete_graph(5)
+
+        g.add_node(10)
+
+        g.remove_edge(0, 4)
+        g.remove_edge(1, 2)
+        g.remove_edge(2, 3)
+        g.remove_edge(3, 4)
+
+        for e in g.edges:
+            if e[0] == 0:
+                g[e[0]][e[1]]["weight"] = 25
+            else:
+                g[e[0]][e[1]]["weight"] = 1
+
+        disparities = edge_disparity(g)
+
+        correct_result = {
+            (0, 1): 0.0054869684499314125,
+            (0, 2): 0.038461538461538436,
+            (0, 3): 0.038461538461538436,
+            (1, 3): 0.9272976680384089,
+            (1, 4): 0.5,
+            (2, 4): 0.5
+        }
+
+        assert correct_result == disparities
