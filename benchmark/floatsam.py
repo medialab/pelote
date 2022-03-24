16e929b6b8a158abe8d9260bc3d536c24ef948b8
Yomguithereal <guillaumeplique@gmail.com>
2022-03-24 10:42:46 +0100

    Testing floatsam

--- benchmark/floatsam.py
+++ benchmark/floatsam.py
@@ -7,12 +7,18 @@ from pelote import (
     floatsam_threshold_learner,
 )
 
+from pelote.graph import largest_connected_component_order
+
 with open("data/bipartite2.csv") as f:
     bipartite = table_to_bipartite_graph(csv.DictReader(f), "account", "post")
 
 monopartite = monopartite_projection(bipartite, "account", metric="jaccard")
 
+print(monopartite.order(), largest_connected_component_order(monopartite))
+
 with Timer("floatsam"):
-    threshold = floatsam_threshold_learner(monopartite, on_epoch=print)
+    threshold = floatsam_threshold_learner(
+        monopartite, on_epoch=print, max_drifter_order=25
+    )
 
 print("Found threshold:", threshold)
