f2e6a74c5840c81cc481534fefd14c8c1c7ae3ba
Yomguithereal <guillaumeplique@gmail.com>
2022-03-24 09:45:56 +0100

    Fixing issue related to bonkers pypi naming for typing-extensions Fix #28

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
     python_requires=">=3.6",
     packages=find_packages(exclude=["benchmark", "docs", "test"]),
     package_data={"docs": ["README.md"], "pelote": ["*.pyi"]},
-    install_requires=["networkx>=2,<3", "typing_extensions>=4"],
+    install_requires=["networkx>=2,<3", "typing-extensions>=4"],
     zip_safe=True,
 )
