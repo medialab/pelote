5740bba8e21e63e559cb2d84e7d831ac09ff8d1f
bmaz <beatrice.mazoyer@gmail.com>
2022-04-01 15:38:20 +0200

    #41 pandas zip vs itertuples full test

--- experiments/pandas_iterate.py
+++ experiments/pandas_iterate.py
@@ -1,6 +1,14 @@
 from ebbe import Timer
 import pandas as pd
 
+"""
+Experiment on pandas dataframe iteration speed:
+iter with iterrows: 3s, 946ms
+iter with itertuples and get dicts: 120ms, 302µs
+iter with zip and get dicts: 60ms, 224µs
+iter with itertuples and get tuples: 111ms, 277µs
+iter with zip and get tuples: 34ms, 987µs
+"""
 
 diamonds = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv')
 
@@ -14,7 +22,8 @@ def iter_iterrows(df, columns):
         rows.append([row[col] for col in columns])
     return rows
 
-def iter_itertuples(df, columns):
+
+def iter_tuples_itertuples(df, columns):
     rows = []
     table = df[columns].itertuples(index=False)
     columns = range(len(columns))
@@ -23,10 +32,26 @@ def iter_itertuples(df, columns):
     return rows
 
 
-def iter_zip(df, columns):
+def iter_tuples_zip(df, columns):
     rows = []
     table = zip(*(df[col].values for col in columns))
     columns = range(len(columns))
+    for row in table:
+        rows.append([row[col] for col in columns])
+    return rows
+
+
+def iter_dict_itertuples(df, columns):
+    rows = []
+    table = (dict(zip(columns, row)) for row in df[columns].itertuples(index=False))
+    for row in table:
+        rows.append([row[col] for col in columns])
+    return rows
+
+
+def iter_dict_zip(df, columns):
+    rows = []
+    table = (dict(zip(columns, row)) for row in zip(*(df[col].values for col in columns)))
     for row in table:
         rows.append([row[col] for col in columns])
     return rows
@@ -36,12 +61,22 @@ with Timer("iter with iterrows"):
     a = iter_iterrows(diamonds, data)
 
 
-with Timer("iter with itertuples"):
-    c = iter_itertuples(diamonds, data)
+with Timer("iter with itertuples and get dicts"):
+    b = iter_dict_itertuples(diamonds, data)
 
 
-with Timer("iter with zip"):
-    b = iter_zip(diamonds, data)
+with Timer("iter with zip and get dicts"):
+    c = iter_dict_zip(diamonds, data)
+
+
+with Timer("iter with itertuples and get tuples"):
+    d = iter_tuples_itertuples(diamonds, data)
+
+
+with Timer("iter with zip and get tuples"):
+    e = iter_tuples_zip(diamonds, data)
 
 assert a == b
 assert a == c
+assert a == d
+assert a == e
