69971ccf8919287c12d62b33b21b3a1a2be33ff0
Yomguithereal <guillaumeplique@gmail.com>
2022-06-07 15:06:37 +0200

    Bench

--- experiments/edge_disparity.py
+++ experiments/edge_disparity.py
@@ -6,7 +6,8 @@ from pelote import table_to_bipartite_graph, edge_disparity, monopartite_project
 with open("data/bipartite2.csv") as f:
     bipartite = table_to_bipartite_graph(csv.DictReader(f), "account", "post")
 
-monopartite = monopartite_projection(bipartite, "account", metric="jaccard")
+with Timer("projection"):
+    monopartite = monopartite_projection(bipartite, "account", metric="jaccard")
 
 print(monopartite.order(), monopartite.size())
 
