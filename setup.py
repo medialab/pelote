c7a0bd2132e225cd00bd4e89916ec661fdad86cb
Yomguithereal <guillaumeplique@gmail.com>
2023-09-08 14:03:45 +0200

    Bump 0.8.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as f:
 
 setup(
     name="pelote",
-    version="0.8.1",
+    version="0.8.2",
     description="Collection of network-related utilities for python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
